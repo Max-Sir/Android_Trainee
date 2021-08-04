fa8d849ed0022dd2ed186ee3eff392753b4ea2e6
Max-Sir <maksimsyromolotov@coherentsolutions.com>
2021-08-04 15:25:18 +0300

    1. Android Studio, структура проекта, modules, tests https://developer.android.com/training/basics/firstapp/creating-project - Создать новый проект (Empty Activity, Use Room, Include Unit/UI Tests) - Настроить target (Compile SDK – 30, Min SDK - 24) - Настроить иконку апп (все поддерживаемые размеры картинок) - Настроить подпись под иконкой апп (к текущему название добавить в начало и конец символ $, e.g. ${project_name}$)

--- Task1/app/src/main/java/com/coherentsolutions/max/sir/task1/MainActivity.kt
+++ Task1/app/src/main/java/com/coherentsolutions/max/sir/task1/MainActivity.kt
@@ -6,5 +6,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        R.string.app_name
     }
 }
