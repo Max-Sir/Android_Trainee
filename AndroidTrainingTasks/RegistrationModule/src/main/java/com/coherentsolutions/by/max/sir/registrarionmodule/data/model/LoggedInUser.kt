04a6e1e41049a485e067758c0c8a11e8fde2a2b4
Max-Sir <maksimsyromolotov@coherentsolutions.com>
2021-08-09 14:37:33 +0300

    no message

--- AndroidTrainingTasks/RegistrationModule/src/main/java/com/coherentsolutions/by/max/sir/registrarionmodule/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.coherentsolutions.by.max.sir.registrarionmodule.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
