d6a34f17c0ca5654a326ffcdf9e93f5bb18add08
Max-Sir <maksimsyromolotov@coherentsolutions.com>
2021-09-07 10:03:51 +0300

    edited activity_login.xml to make it more responsive and use material design

--- AndroidTrainingTasks/app/src/main/java/com/coherentsolutions/by/max/sir/androidtrainingtasks/network/RetrofitService.kt
+++ AndroidTrainingTasks/app/src/main/java/com/coherentsolutions/by/max/sir/androidtrainingtasks/network/RetrofitService.kt
@@ -1,10 +1,15 @@
 package com.coherentsolutions.by.max.sir.androidtrainingtasks.network
 
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface RetrofitService {
     @POST(value = "user")
     suspend fun createUser():Call<ServerResponse>
     // TODO('''@GET and @DELETE)
+
+    @GET(value="user")
+    suspend fun getUsers()
+
 }
