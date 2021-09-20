377cb35df108cb92f3c2a5f9cb4ea0a00e2a7f48
Maxim <syromolotov.maks@mail.ru>
2021-09-20 11:00:33 +0300

    OkHttpLoggingInterceptor added logging of network service by built in feature of framework next step is finally try to fix swaggers bug if it's possible and make PR

--- AndroidTrainingTasks/app/src/main/java/com/coherentsolutions/by/max/sir/androidtrainingtasks/network/PetstoreService.kt
+++ AndroidTrainingTasks/app/src/main/java/com/coherentsolutions/by/max/sir/androidtrainingtasks/network/PetstoreService.kt
@@ -1,9 +1,10 @@
 package com.coherentsolutions.by.max.sir.androidtrainingtasks.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
 object PetstoreService {
 
     const val API_KEY = "hekko"
@@ -11,12 +12,23 @@ object PetstoreService {
     const val SERVER_TAG = "pet-store"
     const val BASE_URL = "https://petstore.swagger.io/v2/"
 
+    private val client:OkHttpClient
+
+    val interceptor = HttpLoggingInterceptor()
+    init {
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+    }
+
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .baseUrl(BASE_URL)
             .build()
     }
 
 }
+
